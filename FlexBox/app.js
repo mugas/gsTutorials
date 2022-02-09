5638381384eb38a7929e144c4b02963075ad6d5e
mugas <ricardo.d.moreira.rm@gmail.com>
2022-02-10 00:38:01 +0200

    finish justify content

--- FlexBox/app.js
+++ FlexBox/app.js
@@ -1,5 +1,49 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
+        const flexEnd = document.querySelector('.flex_end');
+        const flexCenter = document.querySelector('.center');
+        const flexSpaceBetween = document.querySelector('.space_between');
+        const flexSpaceAround = document.querySelector('.space_around');
+        const flexSpaceEvenly = document.querySelector('.space_evenly');
+
+
+        if (entry.isIntersecting) {
+            flexEnd.classList.add('flex_end_animation');
+            flexCenter.classList.add('flex_center_animation');
+            flexSpaceBetween.classList.add('flex_space_between_animation');
+            flexSpaceAround.classList.add('flex_space_around_animation');
+            flexSpaceEvenly.classList.add('flex_space_evenly_animation');
+            return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        flexEnd.classList.remove('flex_end_animation');
+        flexCenter.classList.remove('flex_center_animation');
+        flexSpaceBetween.classList.remove('flex_space_between_animation');
+        flexSpaceAround.classList.remove('flex_space_around_animation');
+        flexSpaceEvenly.classList.remove('flex_space_evenly_animation');
+    });
+});
+
+observer.observe(document.querySelector('.justify_content'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
         const firstFlex = document.querySelector('.flex_end');
 
 
@@ -31,4 +75,5 @@ const secondObserver = new IntersectionObserver(entries => {
     });
 });
 
-secondObserver.observe(document.querySelector('.initial'));
+secondObserver.observe(document.querySelector('.initial')); */
+
