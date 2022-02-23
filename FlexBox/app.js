253882c608a5a1241b1d19dc3ee1588d0be4bb12
mugas <ricardo.d.moreira.rm@gmail.com>
2022-02-24 00:08:16 +0200

    start adding scripits for buttons

--- FlexBox/app.js
+++ FlexBox/app.js
@@ -1,90 +1,58 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const flexEnd = document.querySelector('.flex_end');
-        const flexCenter = document.querySelector('.center');
-        const flexSpaceBetween = document.querySelector('.space_between');
-        const flexSpaceAround = document.querySelector('.space_around');
-        const flexSpaceEvenly = document.querySelector('.space_evenly');
-        const flexAlignEnd = document.querySelector('.flex_align_end');
-        const flexAlignCenter = document.querySelector('.flex_align_center');
-        const flexAlignStretch = document.querySelector('.flex_align_stretch');
-        const flexAlignBaseline = document.querySelector('.flex_align_baseline');
+const rowReverse = document.querySelector('.row_reverse_button');
+const rowReverseAnimation = document.querySelector('.row_reverse');
+const columnButtton = document.querySelector('.column_button');
+const columnAnimation = document.querySelector('.column');
+const columnRreverseButton = document.querySelector('.column_reverse_button');
+const columnReverseAnimation = document.querySelector('.column_reverse');
+const justifyFlexEndButton = document.querySelector('.flex_end_button');
+const flexEndAnimation = document.querySelector('.flex_end');
+const justifyCenterButton = document.querySelector('.justify_center_button');
+const justifyCenterAnimation = document.querySelector('.center');
 
-        if (entry.isIntersecting) {
-            flexEnd.classList.add('flex_end_animation');
-            flexCenter.classList.add('flex_center_animation');
-            flexSpaceBetween.classList.add('flex_space_between_animation');
-            flexSpaceAround.classList.add('flex_space_around_animation');
-            flexSpaceEvenly.classList.add('flex_space_evenly_animation');
-            flexAlignEnd.classList.add('flex_align_end_animation');
-            flexAlignCenter.classList.add('flex_align_center_animation');
-            flexAlignStretch.classList.add('flex_align_stretch_animation');
-            flexAlignBaseline.classList.add('flex_align_baseline_animation');
-            return; // if we added the class, exit the function
-        }
+const anima = document.querySelector('.anima');
+const animaBem = document.querySelector('.animabem')
 
-        // We're not intersecting, so remove the class!
-        flexEnd.classList.remove('flex_end_animation');
-        flexCenter.classList.remove('flex_center_animation');
-        flexSpaceBetween.classList.remove('flex_space_between_animation');
-        flexSpaceAround.classList.remove('flex_space_around_animation');
-        flexSpaceEvenly.classList.remove('flex_space_evenly_animation');
-        flexAlignEnd.classList.remove('flex_align_end_animation');
-        flexAlignCenter.classList.remove('flex_align_center_animation');
-        flexAlignStretch.classList.remove('flex_align_stretch_animation');
-        flexAlignBaseline.classList.remove('flex_align_baseline_animation');
-    });
+//Event Listener of Flex Direction
+rowReverse.addEventListener('click', e => {
+    rowReverseAnimation.classList.add('row_reverse_animation')
+    console.log("hello")
 });
 
-observer.observe(document.querySelector('.justify_content'));
+columnButtton.addEventListener('click', e => {
+    columnAnimation.classList.add('column_animation')
+})
 
+columnRreverseButton.addEventListener('click', e => {
+    columnReverseAnimation.classList.add('column_reverse_animation')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const firstFlex = document.querySelector('.flex_end');
-
-
-        if (entry.isIntersecting) {
-            firstFlex.classList.add('flex_end_animation');
-            return; // if we added the class, exit the function
-        }
-
-        // We're not intersecting, so remove the class!
-        firstFlex.classList.remove('flex_end_animation');
-    });
+rowReverse.addEventListener('click', e => {
+    rowReverseAnimation.classList.add('row_reverse_animation')
 });
 
-observer.observe(document.querySelector('.initial'));
+//Event Listeners of Justify Content
 
-
-const secondObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        const secondFlex = document.querySelector('.center');
-
-
-        if (entry.isIntersecting) {
-            secondFlex.classList.add('flex_center_animation');
-            return; // if we added the class, exit the function
-        }
-
-        // We're not intersecting, so remove the class!
-        secondFlex.classList.remove('flex_center_animation');
-    });
+justifyFlexEndButton.addEventListener('click', e => {
+    flexEndAnimation.classList.add('flex_end_animation')
 });
 
-secondObserver.observe(document.querySelector('.initial')); */
+
+justifyCenterButton.addEventListener('click', e => {
+    justifyCenterAnimation.classList.add('flex_center_animation')
+});
+
+
+
+anima.addEventListener('click', e => {
+    console.log("hello")
+    animaBem.classList.add('flex_align_content_space_evenly')
+})
+
+
+
+
+
+
+
+
 
